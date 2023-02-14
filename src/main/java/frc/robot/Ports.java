893e462d86abdf7d5eac21912b639e9caa862ef2
Dave Wilson <dave@webartz.com>
2023-02-14 17:35:23 -0600

    Added some TODOs in Ports.java

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,6 +4,11 @@ import frc.robot.utils.CANPort;
 
 public class Ports
 {
+    // TODO: Whenever you add a CAN ID to the file, actually assign a number. A
+    // definition of 0 is definitely wrong and the choice is essentially
+    // arbitrary so why create a job that someone needs to remember to do later
+    // when you could just stick in any non-clashing value when the port is 
+    // initially added?
     public static class ArmPorts
     {
         private static final String busName      = "";
@@ -14,10 +19,19 @@ public class Ports
 
     public static class OperatorPorts
     {
+        // TODO: What do these "ports" represent? I presume they are Joystick
+        // IDs?
         public static final int kDriverControllerPort   = 0;
         public static final int kOperatorControllerPort = 1;
     }
 
+    // TODO: I would have only one Arm class here and include the superset of IDs
+    // required to support either the high or low variants. Having two classes 
+    // makes it likely that we'll legitimately have duplicated CAN IDs in the file
+    // and that's potentially confusing.
+    // Actually, I'm even more confused that we have 3 Arm classes already. What
+    // differentiates between ArmPorts and the two possible implementations,
+    // HighArm and LowArm? Consolidate everything under ArmPorts, I suggest.
     public static class HighArmPorts
     {
         private static final String busName        = "";
