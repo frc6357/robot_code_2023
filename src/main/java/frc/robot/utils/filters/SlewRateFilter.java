4f93ffbe923c1baffe23f04d1a3b30f74b531d11
Shaun Kurian <shaunkurian345@gmail.com>
2023-02-21 17:09:24 -0600

    Updated slew rate filter to handle a deadband

--- src/main/java/frc/robot/utils/filters/SlewRateFilter.java
+++ src/main/java/frc/robot/utils/filters/SlewRateFilter.java
@@ -9,18 +9,27 @@ public class SlewRateFilter implements Filter
 {
 
     private final SlewRateLimiter slewFilter;
-    private double                reversed = 1.0;
+    private double                reversed;
+    private double                deadband;
+    private double                gain;
+    private double                slope;
 
     /**
      * Creates a filter with a max specified change rate. This prevents an input from
      * changing too quickly.
      * 
      * @param filterRate
-     *            The max rate at which the input can change
+     *            The max rate at which the input can change in units per second
+     * @param deadband
+     *            Used as an area around the zero of the controller inputs. Should be a
+     *            positive value. This is used to stop natural controller drift and small
+     *            accidental inputs. Deadband is measured in percent of raw axis value.
+     * @param reversed
+     *            Whether the sign of the input should be inverted
      */
-    public SlewRateFilter(double filterRate)
+    public SlewRateFilter(double filterRate, double deadband, boolean reversed)
     {
-        slewFilter = new SlewRateLimiter(filterRate);
+        this(filterRate, 1.0, deadband, reversed);
     }
 
     /**
@@ -28,14 +37,25 @@ public class SlewRateFilter implements Filter
      * changing too quickly.
      * 
      * @param filterRate
-     *            The max rate at which the input can change
+     *            The max rate at which the input can change in units per second
+     * @param gain
+     *            The max value that should be output by the axis after filtering. This is
+     *            multiplied the raw axis to get the final value
+     * @param deadband
+     *            Used as an area around the zero of the controller inputs. Should be a
+     *            positive value. This is used to stop natural controller drift and small
+     *            accidental inputs.
      * @param reversed
      *            Whether the sign of the input should be inverted
      */
-    public SlewRateFilter(double filterRate, boolean reversed)
+    public SlewRateFilter(double filterRate, double gain, double deadband, boolean reversed)
     {
         slewFilter = new SlewRateLimiter(filterRate);
         this.reversed = reversed ? -1.0 : 1.0;
+        this.deadband = deadband;
+        this.gain = gain;
+
+        slope = 1 / (1 - this.deadband);
     }
 
     /**
@@ -48,7 +68,14 @@ public class SlewRateFilter implements Filter
      */
     public double filter(double rawAxis)
     {
-        return reversed * slewFilter.calculate(rawAxis);
+        if (Math.abs(rawAxis) < deadband)
+        {
+            return 0.0;
+        }
+        else
+        {
+            return reversed * slewFilter.calculate(gain * (rawAxis - deadband) * slope);
+        }
     }
 
     /**
