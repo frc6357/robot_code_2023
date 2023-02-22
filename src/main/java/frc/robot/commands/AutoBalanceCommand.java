da128e3aa7cd3fd72dbcd6d79e56ffbb9d308846
Shaun Kurian <shaunkurian345@gmail.com>
2023-02-21 18:50:31 -0600

    Functioning autobalance...needs fine tuning

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -3,20 +3,20 @@ package frc.robot.commands;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SK23Drive;
-import frc.robot.utils.filters.FilteredJoystick;
 
 public class AutoBalanceCommand extends CommandBase
 {
     private SK23Drive     subsystem;
     private PIDController xPID;
     private PIDController yPID;
+    private double maxSpeed = 0.5;
 
-    public AutoBalanceCommand(FilteredJoystick controller, SK23Drive drive)
+    public AutoBalanceCommand(SK23Drive drive)
     {
         this.subsystem = drive;
 
-        xPID = new PIDController(0, 0, 0, 0.02);
-        yPID = new PIDController(0, 0, 0, 0.02);
+        xPID = new PIDController(0.05, 0, 0, 0.02);
+        yPID = new PIDController(0.05, 0, 0, 0.02);
 
         addRequirements(subsystem);
     }
@@ -35,11 +35,16 @@ public class AutoBalanceCommand extends CommandBase
         // A positive pitch requires a positive x speed.
         // A positive measurement will return a negative input,
         // which is the opposite sign of what is expected.
-        double xSpeed = -xPID.calculate(subsystem.getPitch());
+        double xSpeed = xPID.calculate(subsystem.getPitch());
         // A positive roll requires a negative y speed.
         // A positive measurement will return a negative input,
         // which is the same sign as what is expected
-        double ySpeed = yPID.calculate(subsystem.getRoll());
+        double ySpeed = -yPID.calculate(subsystem.getRoll());
+
+        // double totalSpeed = Math.hypot(xSpeed, ySpeed);
+        // double scaleFactor = totalSpeed > maxSpeed ? maxSpeed / totalSpeed : 1;
+        // xSpeed *= scaleFactor;
+        // ySpeed *= scaleFactor;
 
         // Use the PID controllers to control the robot relative to itself,
         // NOT in field relative mode, as it is using robot angles.
