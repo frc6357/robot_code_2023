5f1aa90c534df7bc8796a4dfe1d88088d71d2613
Justin Garrity <firejustinice3@gmail.com>
2023-04-02 08:15:00 -0500

    fine tuned autobalance pid values

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -32,8 +32,8 @@ public class AutoBalanceCommand extends CommandBase
         this.subsystem = drive;
         this.rotation = rotation;
 
-        xPID = new PIDController(0.03875, 0, 0, 0.02);
-        yPID = new PIDController(0.03875, 0, 0, 0.02);
+        xPID = new PIDController(0.03, 0, 0, 0.02);
+        yPID = new PIDController(0.03, 0, 0, 0.02);
 
         addRequirements(subsystem);
     }
